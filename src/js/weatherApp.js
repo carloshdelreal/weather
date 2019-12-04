38637b9e8197dc452efe89c356a30e0bc5963e8d
Carlos Del Real <carloshdelreal@gmail.com>
2019-12-03 21:09:12 -0500

    fix linting

--- src/js/weatherApp.js
+++ src/js/weatherApp.js
@@ -1,4 +1,21 @@
-import data from './data';
+async function dataGetter(city, unit) {
+  const query = `q=${city}`;
+  let units = 'units=';
+  if (unit === 0) {
+    units += 'metric';
+  } else {
+    units += 'imperial';
+  }
+
+  const apikey = 'appid=117282e70ab56637f9fbaa2e9518192a';
+  const response = await fetch(
+    `http://api.openweathermap.org/data/2.5/weather?${query}&${apikey}&${units}`,
+    { mode: 'cors' }
+  );
+  const myJson = await response.json();
+
+  return { city: myJson.name, country: myJson.sys.country, main: myJson.main };
+}
 
 export default (DOM) => {
   const inputCityDOM = DOM.cityInput;
@@ -53,7 +70,7 @@ export default (DOM) => {
 
   const search = () => {
     const formData = getFormData();
-    data(formData.cityInput, tempUnits).then((promiseData) => {
+    dataGetter(formData.cityInput, tempUnits).then((promiseData) => {
       updateConditions(promiseData);
     });
   };
